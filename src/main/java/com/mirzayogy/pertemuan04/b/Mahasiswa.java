7524265d50f8e8e0ab83c5309d8780dbb674e10f
mirzayogy <mirza.yogy@gmail.com>
2023-05-24 09:20:24 +0800

    membuat class

---
+++ src/main/java/com/mirzayogy/pertemuan04/b/Mahasiswa.java
@@ -0,0 +1,21 @@
+package com.mirzayogy.pertemuan04.b;
+
+public class Mahasiswa {
+    String nama;
+    String alamat;
+    String npm;
+    double tinggiBadan;
+    int usia;
+    
+    void belajar(){
+        System.out.println(nama +" sedang belajar");
+    }
+    
+    void masukKelas(){
+        System.out.println(nama + " masuk kelas");
+    }
+   
+    
+    
+    
+}
--- src/main/java/com/mirzayogy/pertemuan04/b/Main.java
+++ src/main/java/com/mirzayogy/pertemuan04/b/Main.java
@@ -2,5 +2,27 @@ package com.mirzayogy.pertemuan04.b;
 public class Main {
     public static void main(String[] args) {
         
+        Mahasiswa adul;
+        adul = new Mahasiswa();
+        adul.nama = "Adul Bin Kidul";
+        adul.tinggiBadan = 200;
+        adul.npm = "201010101010";
+        adul.usia = 100;
+        
+        adul.belajar();
+        adul.masukKelas();
+        
+        Mahasiswa bejo = new Mahasiswa();
+        
+        bejo.nama = "Bejo bin Lucky";
+        bejo.tinggiBadan = 50;
+        bejo.npm = "20111111";
+        bejo.usia = 50;
+        
+        bejo.belajar();
+        bejo.masukKelas();
+        
+
+        
     }
 }
